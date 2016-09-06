93385965e30540516de08a7f8cb36878337ee2f8
Anton Santiago <kristofercortez21@gmail.com>
2016-09-06 10:22:02 +0800

    created register and login page w/o functions yet

--- web/router.ex
+++ web/router.ex
@@ -1,9 +1,17 @@
 defmodule Peepchat.Router do
+  
   use Peepchat.Web, :router
 
   pipeline :api do
     plug :accepts, ["json", "json-api"]
   end
+
+  pipeline :api_auth do
+    plug :accepts, ["json", "json-api"]
+    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.LoadResource
+  end
+
 
   scope "/api", Peepchat do
     pipe_through :api
@@ -11,6 +19,11 @@ defmodule Peepchat.Router do
     post "register", RegistrationController, :create
     # Login
     post "token", SessionController, :create, as: :login
+  end
+
+  scope "/api", Peepchat do
+    pipe_through :api_auth
+    get "/user/current", UserController, :current
   end
   
 end
