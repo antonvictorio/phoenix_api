0f637e4f206566f3c485711c4bd8c5fd72a82d6b
Anton Santiago <kristofercortez21@gmail.com>
2016-09-07 14:52:22 +0800

    updated heroku

--- config/prod.exs
+++ config/prod.exs
@@ -2,14 +2,14 @@ use Mix.Config
 
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "obscure-ocean-19966.herokuapp.com/", port: 443],
-  secret_key_base: System.get_env("ePgYNZ6CYWSkhN/YmR10Q78orMwTq5jDCmeg23VFHTlF2AMc8MkoF1GOMGfG8HNd"),
+  url: [scheme: "https", host: "radiant-crag-90533.herokuapp.com", port: 443],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
 
 config :peepchat, Peepchat.Repo,
   adapter: Ecto.Adapters.Postgres,
   #url: System.get_env("postgres://bxouuucjmhlhab:W8UErdh_IOUqjhxdXjq7vApN-H@ec2-174-129-223-35.compute-1.amazonaws.com:5432/de3e8aprn5k39d"),
-  url: System.get_env("postgres://yrhfashceavqbn:I8g21lwIhlVUawyYtZUP7DUnwc@ec2-54-163-239-12.compute-1.amazonaws.com:5432/d310gr91tcc3lo"),
+  url: System.get_env("DATABASE_URL"),
   pool_size: 20
 
 # Do not print debug messages in production
